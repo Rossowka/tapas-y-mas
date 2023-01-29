56a9a89bc67dd0f769d8628e9d691a7d7d64db60
Sara Rossow <rossowka@icloud.com>
2023-01-30 00:14:29 +0100

    add: drawer menu for mobile

--- src/components/DrawerNavBar.js
+++ src/components/DrawerNavBar.js
@@ -10,11 +10,14 @@ import {
 	Divider,
 	List,
 	IconButton,
+	Drawer,
 } from "@mui/material";
 
 import MenuIcon from "@mui/icons-material/Menu";
 
 import Logo from "./Logo";
+
+const drawerWidth = 240;
 
 function DrawerNavBar() {
 	const [mobileOpen, setMobileOpen] = useState(false);
@@ -25,63 +28,81 @@ function DrawerNavBar() {
 
 	const drawer = (
 		<Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-			<Logo />
-			<Divider />
 			<List></List>
 		</Box>
 	);
 
 	return (
-		<AppBar
-			component="nav"
-			sx={{
-				background: "rgba(0,0,0, 0.25)",
-			}}
-		>
-			<Container>
-				<Toolbar
+		<Box>
+			<AppBar
+				component="nav"
+				sx={{
+					background: "rgba(0,0,0, 0.25)",
+				}}
+			>
+				<Container>
+					<Toolbar
+						sx={{
+							justifyContent: "space-between",
+						}}
+					>
+						<Logo />
+						<Stack
+							direction="row"
+							spacing={2}
+							sx={{
+								textShadow: "1px 1px 1px rgb(0,0,0,0.5)",
+								display: { xs: "none", sm: "inherit" },
+							}}
+						>
+							<Link href="#" color="inherit" underline="none">
+								Menu
+							</Link>
+							<Link href="#" color="inherit" underline="none">
+								Services
+							</Link>
+							<Link href="#" color="inherit" underline="none">
+								Our Story
+							</Link>
+							<Link href="#" color="inherit" underline="none">
+								Visit Us
+							</Link>
+							<Link href="#" color="inherit" underline="none">
+								Careers
+							</Link>
+						</Stack>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={handleDrawerToggle}
+							sx={{
+								display: { sm: "none" },
+								zIndex: 9999
+							}}
+						>
+							<MenuIcon />
+						</IconButton>
+					</Toolbar>
+				</Container>
+			</AppBar>
+			<Box component="nav">
+				<Drawer
+					variant="temporary"
+					anchor='right'
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true, // Better open performance on mobile.
+					}}
 					sx={{
-						justifyContent: "space-between",
+						display: { xs: "block", sm: "none" },
+						'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
 					}}
 				>
-					<Logo />
-					<Stack
-						direction="row"
-						spacing={2}
-						sx={{
-							textShadow: "1px 1px 1px rgb(0,0,0,0.5)",
-							display: { xs: "none", sm: "inherit" },
-						}}
-					>
-						<Link href="#" color="inherit" underline="none">
-							Menu
-						</Link>
-						<Link href="#" color="inherit" underline="none">
-							Services
-						</Link>
-						<Link href="#" color="inherit" underline="none">
-							Our Story
-						</Link>
-						<Link href="#" color="inherit" underline="none">
-							Visit Us
-						</Link>
-						<Link href="#" color="inherit" underline="none">
-							Careers
-						</Link>
-					</Stack>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						onClick={handleDrawerToggle}
-						sx={{
-							display: { sm: "none" },
-						}}
-					>
-						<MenuIcon />
-					</IconButton>
-				</Toolbar>
-			</Container>
-		</AppBar>
+					{drawer}
+				</Drawer>
+			</Box>
+		</Box>
 	);
 }
 
