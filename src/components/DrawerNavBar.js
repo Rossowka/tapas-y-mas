cd6f91290d2e841c7dda50c3fab692770305873b
Sara Rossow <rossowka@icloud.com>
2023-01-29 23:23:42 +0100

    add: responsive manu button

--- src/components/DrawerNavBar.js
+++ src/components/DrawerNavBar.js
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { AppBar, Container, Stack, Toolbar, Link } from "@mui/material";
+import {
+	AppBar,
+	Container,
+	Stack,
+	Toolbar,
+	Link,
+	Box,
+	Divider,
+	List,
+	IconButton,
+} from "@mui/material";
+
+import MenuIcon from "@mui/icons-material/Menu";
 
 import Logo from "./Logo";
 
 function DrawerNavBar() {
+	const [mobileOpen, setMobileOpen] = useState(false);
+
+	const handleDrawerToggle = () => {
+		setMobileOpen((prevState) => !prevState);
+	};
+
+	const drawer = (
+		<Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+			<Logo />
+			<Divider />
+			<List></List>
+		</Box>
+	);
 
 	return (
 		<AppBar
+			component="nav"
 			sx={{
 				background: "rgba(0,0,0, 0.25)",
 			}}
 		>
 			<Container>
-				<Toolbar>
-					<Link
-							sx={{
-								flexGrow: 1,
-								color: "inherit",
-								height: 40,
-							}}
-						>
-						<Logo />
-					</Link>
-					<Stack direction="row" spacing={2}>
+				<Toolbar
+					sx={{
+						justifyContent: "space-between",
+					}}
+				>
+					<Logo />
+					<Stack
+						direction="row"
+						spacing={2}
+						sx={{
+							textShadow: "1px 1px 1px rgb(0,0,0,0.5)",
+							display: { xs: "none", sm: "inherit" },
+						}}
+					>
 						<Link href="#" color="inherit" underline="none">
 							Menu
 						</Link>
@@ -40,6 +69,16 @@ function DrawerNavBar() {
 							Careers
 						</Link>
 					</Stack>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						onClick={handleDrawerToggle}
+						sx={{
+							display: { sm: "none" },
+						}}
+					>
+						<MenuIcon />
+					</IconButton>
 				</Toolbar>
 			</Container>
 		</AppBar>
