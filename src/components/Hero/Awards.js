a8e68f8419d076d3333344079ea994c5cc6376c3
Sara Rossow <rossowka@icloud.com>
2023-02-02 22:00:59 +0100

    add: made component responsive

--- src/components/Hero/Awards.js
+++ src/components/Hero/Awards.js
@@ -1,16 +1,28 @@
 import { Avatar, Container, Stack } from "@mui/material";
-import { awardsItems } from "./consts/awardsItems"
+import { awardsItems } from "./consts/awardsItems";
 
 export default function Awards({ sx }) {
 	return (
-    <Container sx={sx}>
-      <Container>
-        <Stack direction="row" spacing={4}>
-          {awardsItems.map((item) => (
-            <Avatar key={item.img} src={item.img} alt={item.title} sx={{ width: 80, height: 80, backgroundColor: "#fff"}}/>
-            ))}
-        </Stack>
-      </Container>
-    </Container>
+		<Container sx={sx}>
+			<Container>
+				<Stack
+					direction="row"
+					spacing={{ sm: 4 }}
+					sx={{
+						alignItems: { xs: "center", md: "flex-start" },
+						justifyContent: { xs: "space-between", sm: "flex-start" },
+					}}
+				>
+					{awardsItems.map((item) => (
+						<Avatar
+							key={item.img}
+							src={item.img}
+							alt={item.title}
+							sx={{ width: 70, height: 70, backgroundColor: "#fff" }}
+						/>
+					))}
+				</Stack>
+			</Container>
+		</Container>
 	);
 }
