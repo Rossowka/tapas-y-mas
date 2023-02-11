046be4594a732a8fa42ed5b61072c48de0ffdeab
Sara Rossow <rossowka@icloud.com>
2023-02-11 16:49:29 +0100

    add: link hover style

--- src/data/theme.js
+++ src/data/theme.js
@@ -1,32 +1,45 @@
-import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
-export let theme = responsiveFontSizes(createTheme({
-	palette: {
-		primary: {
-			main: "rgb(195,65,61)",
+export let theme = responsiveFontSizes(
+	createTheme({
+		palette: {
+			primary: {
+				main: 'rgb(195,65,61)',
+			},
+			secondary: {
+				main: 'rgb(24,242,178)',
+			},
+			background: {
+				default: 'rgb(49, 49, 49)',
+			},
 		},
-		secondary: {
-			main: "rgb(24,242,178)",
-		},
-		background: {
-			default: "rgb(49, 49, 49)"
-		}
-	},
-}));
+	})
+);
 
 theme = createTheme(theme, {
 	typography: {
 		h2: {
-			fontFamily: "Comicool",
+			fontFamily: 'Comicool',
 			[theme.breakpoints.down('sm')]: {
-				fontSize: "9vw",
-			}
+				fontSize: '9vw',
+			},
 		},
 		h5: {
-			fontFamily: "Comicool",
+			fontFamily: 'Comicool',
 			[theme.breakpoints.down('sm')]: {
-				fontSize: "3.5vw",
-			}
-		}
-	}
+				fontSize: '3.5vw',
+			},
+		},
+	},
+	components: {
+		MuiLink: {
+			styleOverrides: {
+				root: {
+					'&:hover': {
+						borderBottom: `1px solid ${theme.palette.primary.main}`,
+					},
+				},
+			},
+		},
+	},
 });
