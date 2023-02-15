37112b11d1f0acca828cff195cd730f445feea53
Sara Rossow <rossowka@icloud.com>
2023-02-15 16:57:49 +0100

    changed default text color to white

--- src/components/Cards/MenuCard.js
+++ src/components/Cards/MenuCard.js
@@ -1,4 +1,4 @@
-import { OpenInNew } from "@mui/icons-material";
+import { OpenInNew } from '@mui/icons-material';
 import {
 	Box,
 	Card,
@@ -7,67 +7,68 @@ import {
 	CardMedia,
 	Icon,
 	Typography,
-} from "@mui/material";
+} from '@mui/material';
 
-import { grey } from "@mui/material/colors";
-import { ReactComponent as Brand } from "../../assets/img/logo.svg";
+import { grey } from '@mui/material/colors';
+import { ReactComponent as Brand } from '../../assets/img/logo.svg';
 
 export default function MenuCard({ cardItems }) {
 	return (
 		<Card
 			sx={{
 				background: `linear-gradient(315deg, ${grey[300]}, #fff)`,
-				flex: "0 0 40%",
-				boxShadow: "0 30px 60px rgb(0 0 0 / 40%)",
-				position: "relative",
+				flex: '0 0 40%',
+				boxShadow: '0 30px 60px rgb(0 0 0 / 40%)',
+				position: 'relative',
 			}}
 		>
 			<CardActionArea
 				sx={{
-					position: "relative",
+					position: 'relative',
 				}}
 			>
-				<Box sx={{ display: "flex", flexDirection: "column", p: 2 }}>
-					<CardContent sx={{ flex: "1 0 auto", zIndex: 90, mr: 10 }}>
+				<Box sx={{ display: 'flex', flexDirection: 'column', p: 2 }}>
+					<CardContent sx={{ flex: '1 0 auto', zIndex: 90, mr: 10 }}>
 						<Typography
-							component="h4"
-							variant="h4"
-							sx={{ textTransform: "uppercase" }}
+							component='h4'
+							variant='h4'
+							color='black.main'
+							sx={{ textTransform: 'uppercase' }}
 						>
 							{cardItems.title}
 						</Typography>
 					</CardContent>
 					<Icon
-						fontSize="large"
-						aria-label="open menu"
-						sx={{ ml: 2, mb: 2, mt: 5, zIndex: 20, }}
+						fontSize='large'
+						aria-label='open menu'
+						sx={{ ml: 2, mb: 2, mt: 5, zIndex: 20 }}
 					>
-						<OpenInNew fontSize="large" />
+						<OpenInNew fontSize='large' />
 					</Icon>
 				</Box>
 				<Box
 					sx={{
 						color: grey[300],
-						position: "absolute",
+						position: 'absolute',
 						bottom: 0,
 						left: 70,
-						borderRadius: "0 0 4px 0",
+						borderRadius: '0 0 4px 0',
 						zIndex: 0,
-						overflow: "hidden",
+						overflow: 'hidden',
 					}}
 				>
-					<Brand height="50%" />
+					<Brand height='50%' />
 				</Box>
 				<CardMedia
-					component="img"
+					component='img'
 					sx={{
 						width: 150,
-						position: "absolute",
-						bottom: cardItems.bottom,
-						right: cardItems.right,
+						position: 'absolute',
+						bottom: cardItems.positionBottom,
+						right: cardItems.positionRight,
 						zIndex: 10,
 					}}
-					image={cardItems.img}
+					image={cardItems.imgUrl}
 					alt={cardItems.title}
 				/>
 			</CardActionArea>
